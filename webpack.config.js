9278b45d5ecee38e97207408f86e3d6cb1d81c6e
Alexey Kuchyn <alexey.kuchyn@qvantel.com>
2017-08-27 19:04:32 +0300

    Initial commit - POC

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './src'),
+        publicPath: 'js/',
+    },
+    module: {
+        rules: [
+            {
+              test: /\.js$/,
+              exclude: [/node_modules/],
+              use: [{
+                loader: 'babel-loader',
+                options: {
+                  presets: ['es2015'],
+                },
+              }],
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]'
+            }
+        ],
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ],
+};
